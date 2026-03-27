72e42b8573ae905db3eb4054c7d6998402907de7
leohenon <77656081+lhenon999@users.noreply.github.com>
2026-03-27 13:45:52 +0800

    raycast update window scripts

---
+++ .config/raycast/scripts/open-discord.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Focus Discord
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.packageName App Focus
+
+open -a "Discord"
---
+++ .config/raycast/scripts/open-spotify.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Focus Spotify
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.packageName App Focus
+
+open -a "Spotify"
