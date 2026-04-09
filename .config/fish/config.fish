9b8332ea13fe7ae395dcb7e443b3d7008112f229
leohenon <77656081+lhenon999@users.noreply.github.com>
2026-04-09 20:03:54 +0800

    fish update config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,11 @@
+set -g fish_greeting
 set -g VIRTUAL_ENV_DISABLE_PROMPT 1
 
 if status is-interactive
     fish_add_path -g /opt/homebrew/bin /opt/homebrew/sbin
     set -gx PATH /usr/bin /bin /usr/sbin /sbin $PATH
+    set -Ux fish_user_paths ~/go/bin $fish_user_paths
+    fish_add_path -g --move --prepend /opt/homebrew/opt/python@3.10/bin
 
     function prompt_hostname
         echo -n mac
@@ -45,45 +48,39 @@ if status is-interactive
     end
 
     function fish_mode_prompt
+    end
+
+    function fish_prompt
+        set -l st $status
         _lh_load_theme_colors
-        set -l c_fg "#FEFEFE"
-        set -l c_muted "#A0A0A0"
-        set -l c_mint "#99FFE4"
-        set -l c_orange "#FFCFA8"
         set -l c_red "#FF8080"
-        if set -q LH_COLOR_FG
-            set c_fg $LH_COLOR_FG
+        set -l c_muted "#A0A0A0"
+        set -l c_orange "#FFCFA8"
+        set -l c_yellow "#FFD84D"
+        if set -q LH_COLOR_RED
+            set c_red $LH_COLOR_RED
         end
         if set -q LH_COLOR_MUTED
             set c_muted $LH_COLOR_MUTED
         end
-        if set -q LH_COLOR_MINT
-            set c_mint $LH_COLOR_MINT
-        end
         if set -q LH_COLOR_ORANGE
             set c_orange $LH_COLOR_ORANGE
-        end
-        if set -q LH_COLOR_RED
-            set c_red $LH_COLOR_RED
         end
 
         set -l branch (prompt_git_branch)
 
-        # venv prefix
         if set -q VIRTUAL_ENV
             set_color blue
             echo -n "("(basename $VIRTUAL_ENV)") "
             set_color normal
         end
 
-        # directory — hide on $HOME with no git branch
         if test "$PWD" != "$HOME" -o -n "$branch"
             set_color $c_muted
             echo -n (basename $PWD)
             set_color normal
         end
 
-        # git branch
         if test -n "$branch"
             echo -n " "
             set_color blue; echo -n "git:("; set_color normal
@@ -91,39 +88,13 @@ if status is-interactive
             set_color blue; echo -n ")"; set_color normal
         end
 
-        # arrow based on mode
-        switch $fish_bind_mode
-            case default
-                set_color $c_red
-                echo -n " <~ "
-            case insert
-                set_color $c_muted
-                echo -n " ~> "
-            case visual
-                set_color $c_orange
-                echo -n " <~ "
-            case replace_one replace
-                set_color $c_red
-                echo -n " <~ "
-                set_color --bold $c_red
-                echo -n "󰛔 "
-            case '*'
-                set_color $c_muted
-                echo -n " ~> "
-        end
-        set_color normal
-    end
-
-    function fish_prompt
-        set -l st $status
         if test $st -ne 0
-            _lh_load_theme_colors
-            set -l c_red "#FF8080"
-            if set -q LH_COLOR_RED
-                set c_red $LH_COLOR_RED
-            end
-            set_color $c_red; echo -n "✘ "; set_color normal
+            set_color $c_red
+        else
+            set_color $c_yellow
         end
+        echo -n " ✘ "
+        set_color normal
     end
     fish_vi_key_bindings
     bind -M insert \t accept-autosuggestion
@@ -164,12 +135,15 @@ if status is-interactive
     abbr -a bunol 'bun run --cwd ~/dev/opencode/packages/opencode dev'
     abbr -a opl   '~/.config/run_opencode_dev.sh'
 
+    set -gx OPENCODE_ENABLE_EXA 1
     set -gx VISUAL vim
     set -gx EDITOR vim
 end
 
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
+set -gx PATH $PATH /Users/leohenon/.lmstudio/bin
+set PATH $PATH /Users/leohenon/.local/bin
 
 if test -f ~/.config/fish/local.fish
     source ~/.config/fish/local.fish
